2c150a90f29438cd4cfe8cd352f0aed7fb610d16
HEE JI <heeji@HEEJI-MacBook-Air.local>
2024-10-22 21:00:31 +0900

    10월22일 이미지불러오기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
+import GridRow from '/Users/heeji/s-clone01-2408/src/component/category/category.js';
+
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <h1>My Grid Example</h1>
+      <GridRow />  {/* GridRow 컴포넌트를 렌더링 */}
+  </div>;
 }
 
 export default App;
