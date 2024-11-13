4a2afb04088255554c2c3f81b1e50c5328954260
04mmii <155520450+04mmii@users.noreply.github.com>
2024-11-13 23:00:44 +0900

    api 연동중

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import List from "./component/list/List";
 import Detail from "./pages/Detail";
 import Header from "./component/header/header.js";
+<<<<<<< Updated upstream
 import accommodations from "./data/data";
+=======
+import GridRow from "./component/category/category.js";
+import { fetchHotels } from "./api/amadeus";
+>>>>>>> Stashed changes
 
 const App = () => {
+  const [accommodations, setAccommodations] = useState([]);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const loadHotels = async () => {
+      try {
+        const data = await fetchHotels("NYC", "2024-11-10", "2024-11-15");
+        setAccommodations(data.data); // 가져온 데이터로 상태 업데이트
+      } catch (error) {
+        console.error("Error fetching accommodations:", error);
+      }
+    };
+    loadHotels();
+  }, []);
 
   return (
     <Routes>
@@ -17,12 +35,19 @@ const App = () => {
             <header>
               <Header />
             </header>
+<<<<<<< Updated upstream
+=======
+            <div classname="Category">
+              <h1>My Grid Example</h1>
+              <GridRow /> {/* GridRow 컴포넌트를 렌더링 */}
+            </div>
+>>>>>>> Stashed changes
             <div className="card-list">
               {accommodations.map((a, i) => {
                 return (
                   <List
-                    onClick={() => navigate("/detail:id")}
-                    key={a}
+                    onClick={() => navigate(`/detail/${a.hotel.hotelId}`)}
+                    key={a.hotel.hotelId}
                     data={a}
                     // accommodations={accommodations[i]}
                   />
